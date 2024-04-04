be341bf472252038d331ab0184cd694698ff39b9
Gjovani872 <127245091+Gjovani872@users.noreply.github.com>
2024-04-04 23:49:03 +0200

    Add files via upload

---
+++ src/projekat/ProgramConn.java
@@ -0,0 +1,20 @@
+package projekat;
+
+public class ProgramConn {
+	private int noOfSubjects;
+
+	public ProgramConn(int noOfSubjects) {
+		super();
+		this.noOfSubjects = noOfSubjects;
+	}
+
+	public int getNoOfSubjects() {
+		return noOfSubjects;
+	}
+
+	public void setNoOfSubjects(int noOfSubjects) {
+		this.noOfSubjects = noOfSubjects;
+	}
+	
+	
+}
